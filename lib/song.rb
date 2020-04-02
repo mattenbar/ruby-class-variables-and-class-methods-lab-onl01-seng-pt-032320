afc971a93f66fc5ac683f22e373ae87b8a5e5b57
matthew enbar <matthew.enbar@gmail.com>
2020-04-02 17:34:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
 
   def self.genre_count
     self.genres
-    @@genres_count = Hash[@@genre_array.map {|x| [x, @@genres]}]
+    @@genres_count = Hash[@@genre_array.map {|x| [x, @@genres.uniq.count]}]
   end
 
 
