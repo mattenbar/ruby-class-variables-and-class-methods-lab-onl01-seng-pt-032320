206171bbdd552e53652a1f4df8bab7527d78fb22
matthew enbar <matthew.enbar@gmail.com>
2020-04-02 15:00:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,19 +2,19 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
+  @@count = 0
   @@generes = []
   @@artists = []
 
   def initialize (name, artist, genre)
-    @@song_count += 1
+    @@count += 1
     @name = name
     @artist = artist
     @genre = genre
   end
 
   def self.count
-    @@song_count
+    @@count
   end
 
   def self.generes
